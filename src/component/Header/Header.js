434148cbe2732da30e83726cef745e1441e66485
pokhbis <prokash748@gmail.com>
2021-11-27 12:02:37 +0600

    transfer

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
             </div>
             <div className="header-text">
                 <h1>Buy The <span className="header-span">Best Puppies</span> !!!</h1>
-                <h4>Choose you  puppy from our collection of 1700 puppies</h4>
+                <h4>Choose you  puppy from our collection of 1200 puppies</h4>
                 <h2>Collection of <span className="header-span">Puppies</span> worth over: $ 1 Million</h2>
 
             </div>
